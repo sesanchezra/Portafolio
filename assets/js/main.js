470d684a81fc0f0f890d71ecdfa47770d1f4e411
Sebastian Sanchez <sesanchezra@unal.edu.co>
2022-05-19 11:05:04 -0500

    Solucion toggle a navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,16 @@
 const navbarBtn = document.getElementById('navbarBtn')
 const navbarNav = document.querySelector('.navbar__nav')
 
+
 navbarBtn.addEventListener('click', function (e) {
-  console.log(navbarNav)
   navbarNav.classList.toggle('active')
 })
 // end navbar btn menu
 
 // Seleccionar navbar item para que al dar click se cierre todo
 
-// const navbarItem= document.querySelector('.navbar__item')
-// console.log(navbarItem);
+navbarNav.addEventListener('click', function (e) {
+    if (e.target.closest('.navbar__item')) {
+        navbarNav.classList.toggle('active')
+    }
+})
